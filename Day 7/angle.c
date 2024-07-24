37aeefac8bb1459c4fade35f3b13e837b3d0c099
madhav8452 <madhavrathod019@gmail.com>
2024-07-24 21:53:48 +0530

    Day_7

---
+++ Day 7/angle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("A => ");
+    scanf("%d", &a);
+
+    printf("B => ");
+    scanf("%d", &b);
+
+    c = 180 - (a + b);
+    printf("C Value is %d", c);
+
+    return 0;
+}
